1053cc0cdd1dd5e669610ef9ecd72360d90120e8
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-15 12:54:49 +0300

    database file

--- db_bluecollar.sql
+++ db_bluecollar.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2023 at 01:16 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Mar 15, 2024 at 10:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,9 +44,12 @@ CREATE TABLE `tblclients` (
 --
 
 INSERT INTO `tblclients` (`ClientID`, `Fname`, `Lname`, `Address`, `ContactNo`, `Status`, `cUsername`, `cPassword`, `PicLoc`) VALUES
-(1, 'Mustafa', 'Ahmed', 'Prestige, Ngong Road', '723266163', 'Confirmed', 'mussys', '1791243dbeab4c30439d9f7478a22ac530819933', ''),
-(2, 'Asha', 'Jamal', 'Nairobi', '911', 'Confirmed', 'aisha', '81fe8bfe87576c3ecb22426f8e57847382917acf', 'client/car.jpg'),
-(3, 'Uhuru', 'ruto', '179 Mertie Crossroad', '263', 'Confirmed', 'customer1', '8263328106ab337b8198c34842eec25da12c89d4', 'client/TEST 2.jpg');
+(1, 'Mustafa', 'Khalif', 'Prestige, Ngong Road', '723266163', 'Confirmed', 'khalif', '1791243dbeab4c30439d9f7478a22ac530819933', ''),
+(2, 'Asha', 'Jamal', 'Nairobi', '0113543911', 'Confirmed', 'aisha', '81fe8bfe87576c3ecb22426f8e57847382917acf', 'client/car.jpg'),
+(3, 'Uhuru', 'ruto', '179 Mertie Crossroad', '0789635263', 'Pending', 'uhuru', '8263328106ab337b8198c34842eec25da12c89d4', 'client/TEST 2.jpg'),
+(4, 'Jumah', 'Issaack', '208, Katani Rd, Syokimau', '0789635263', 'Confirmed', 'Jumah', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'client/lock.jpg'),
+(6, 'Anjolie Norton', 'Sarah Sellers', 'Ea reprehenderit se', 'Consequat Et aute r', 'Pending', 'feworuxejy', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', ''),
+(7, 'Candice Mays', 'Yuli Cruz', 'Sint quas in animi ', 'Dolor dolore deserun', 'Pending', 'sajykisy', 'ac748cb38ff28d1ea98458b16695739d7e90f22d', '');
 
 -- --------------------------------------------------------
 
@@ -99,7 +102,8 @@ CREATE TABLE `tblrequest` (
 
 INSERT INTO `tblrequest` (`RequestID`, `Request`, `Status`, `Remarks`, `ClientID`, `ServiceID`, `USERID`, `Viewed`) VALUES
 (3, 'PLUMBER', 'Confirmed', '', 2, 4, 0, 1),
-(4, 'budget 300kes', 'Confirmed', '', 3, 5, 0, 0);
+(4, 'Budget 300kes, for paint', 'Confirmed', '', 3, 8, 0, 0),
+(5, 'We are looking for a plumber by the end of this week.\r\nBudget: Ke. 10, 000.', 'Pending', 'Request is on process...', 4, 4, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -133,7 +137,7 @@ INSERT INTO `tblserviceprovider` (`ServiceID`, `ServiceName`, `ServiceContact`, 
 (7, 'Ngoroge Wa Stima', '076545678', 'KIKUYU', 0, 0, 'ELECTRICIAN', 'janobe@gmail.com', 'ELECTRICIAN', '5b38e010524446101dd9f45ca84d37d20d33bf08', 'Confirmed', 'services/NGORO.jpg'),
 (8, 'Peter Marangi', '0798765432', 'CBD', 0, 0, 'PAINTER', 'janobe@gmail.com', 'PAINTER', 'f337b26b210feab31cb06381eb7718679e99da90', 'Confirmed', 'services/MARANGI.jpg'),
 (9, 'Mama Makena', '07234567', 'RUIRU', 0, 0, 'GARDENER', 'janobe@gmail.com', 'GARDENER', '31e2db8a953e5b349c9bd5db4b08173218d562fb', 'Confirmed', 'services/MAKENA.jpg'),
-(10, 'test', '723266163', 'Nairobi, Kenya', 36.1027394, -95.92606049999999, 'test', 'ma5755333@gmail.com', 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'Pending', ''),
+(10, 'Test Marangi', '723266163', 'Nairobi, Kenya', 36.1027394, -95.92606049999999, 'Painting, Interior designer!', 'ma5755333@gmail.com', 'Test Marangi', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'Pending', ''),
 (11, 'Kenya Yetu', '722222222', 'Syokimau', 0, 0, 'Interor design', 'blue@colllar.com', 'Kenya', 'f456eb5a21e8f84466481bb318956433dca7b3fb', 'Pending', 'services/img.jpg');
 
 -- --------------------------------------------------------
@@ -156,9 +160,9 @@ CREATE TABLE `tblusers` (
 --
 
 INSERT INTO `tblusers` (`USERID`, `NAME`, `UEMAIL`, `PASS`, `TYPE`, `PicLoc`) VALUES
-(15, 'sad', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', 'users/fb.jpg'),
-(17, 'Admin Asha ', 'asha.001', '81fe8bfe87576c3ecb22426f8e57847382917acf', 'Staff', 'users/img.jpg'),
-(18, 'ASHA', 'admin2', '32e3a82cbf1481d7f7603a4a7e533ed3856e4ca0', 'Staff', '');
+(15, 'Administrator', 'admin', '81fe8bfe87576c3ecb22426f8e57847382917acf', 'Administrator', 'users/fb.jpg'),
+(19, 'Asha Jama', 'user', '5f30576af23a25b7f44fa7f5fdf70325ee389155', 'Administrator', ''),
+(20, 'User Account', 'user001', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'Staff', 'users/car.jpg');
 
 --
 -- Indexes for dumped tables
@@ -208,7 +212,7 @@ ALTER TABLE `tblusers`
 -- AUTO_INCREMENT for table `tblclients`
 --
 ALTER TABLE `tblclients`
-  MODIFY `ClientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ClientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tblfeedback`
@@ -226,7 +230,7 @@ ALTER TABLE `tblrating`
 -- AUTO_INCREMENT for table `tblrequest`
 --
 ALTER TABLE `tblrequest`
-  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tblserviceprovider`
@@ -238,7 +242,7 @@ ALTER TABLE `tblserviceprovider`
 -- AUTO_INCREMENT for table `tblusers`
 --
 ALTER TABLE `tblusers`
-  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
